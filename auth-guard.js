340a6845074e58f770fbd3adf3f1b59d9c32b0b9
Arteversicoding <hanirafg@gmail.com>
2025-08-29 18:18:34 +0700

    Mendesain Ulang Semua, Menambahkan Fitur Edit Profile, dan Menambahkan halaman informasi akun

---
+++ auth-guard.js
@@ -0,0 +1,21 @@
+import { auth } from "./firebase-init.js";
+
+// This script checks if a user is logged in.
+// If not, it redirects to the login page.
+// Add this script to every page you want to protect.
+
+auth.onAuthStateChanged(function(user) {
+  const currentPath = window.location.pathname;
+  // If page name contains 'admin', require admin login page for unauthenticated users
+  const isAdminArea = currentPath.includes('admin-');
+  if (!user) {
+    if (isAdminArea) {
+      if (!currentPath.endsWith('/login.html')) {
+                window.location.href = '/login.html';
+    } else {
+      if (currentPath !== '/login.html' && currentPath !== '/register.html') {
+        window.location.href = '/login.html';
+      }
+    }
+  }
+});
